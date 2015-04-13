b820c20548379708187bf7a4a266e442fa7fa129
koek67 <krishnan.koushik@gmail.com>
2015-04-13 19:49:06 -0400

    small changes

--- js/chart.js
+++ js/chart.js
@@ -37,3 +37,331 @@ $(function () {
         }]
     });
 });
+
+
+$(function () {
+    $('#graph1').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph2').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph3').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph4').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph5').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph6').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph7').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
+
+
+$(function () {
+    $('#graph8').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: true
+        },
+        title: {
+            text: 'Our survey sample distribution'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '75%',
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Survey distribution',
+            data: [
+                ['On-Campus (Freshman housing)', 69.4],
+                ['On-campus (Other housing)', 22.4],
+                ['Off-campus', 8.2]
+            ]
+        }]
+    });
+});
